a74df46fa5b404822a03e077524f9327d5a6d36b
Erumoico42 <33353648+Erumoico42@users.noreply.github.com>
2017-12-15 22:57:00 +0100

    casovac semaforu

---
+++ bcprace/src/prometheus/SemtamforControl.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prometheus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Platform;
+
+/**
+ *
+ * @author Honza
+ */
+public class SemtamforControl {
+    private Timer timer=new Timer();
+    private TimerTask timertask;
+    private List<Semafor> semafory=new ArrayList<>();
+    private boolean run=false;
+    public SemtamforControl() {
+        newSC();
+    }
+    private void newSC()
+    {
+        timertask = new TimerTask() {
+            @Override
+            public void run(){
+                Platform.runLater(() -> {
+                    tick();
+                });
+            }
+        };
+        timer.schedule(timertask, 1000, 1000);
+        
+    }
+    public void end()
+    {
+        timertask.cancel();
+        timer.cancel();
+    }
+    public List<Semafor> getSemafory()
+    {
+        return semafory;
+    }
+    private void tick()
+    {
+        for (Semafor semafor : semafory) {
+            semafor.tick();
+        }
+    }
+
+    public void newSem(Semafor s)
+    {
+        semafory.add(s);
+    }
+}
